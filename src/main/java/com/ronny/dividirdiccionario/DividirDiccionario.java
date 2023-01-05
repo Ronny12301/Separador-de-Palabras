59082e81e6ee1b4524180aeb63a2eea0c8c1f45d
Ronny <ronny12301@gmail.com>
2023-01-05 16:41:35 -0600

    Commit inicial

---
+++ src/main/java/com/ronny/dividirdiccionario/DividirDiccionario.java
@@ -0,0 +1,64 @@
+package com.ronny.dividirdiccionario;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ronny12301
+ */
+public class DividirDiccionario {
+
+    static Map<String, FileWriter> writers = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("/home/ronny12301/Documentos/NetBeansProjects/DividirDiccionario/src/main/java/com/ronny/dividirdiccionario/espanol.txt"))) {
+
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                if (linea.length()<=12) {
+                    escribir(linea + "\n");
+                }
+                else {
+                    System.out.println(linea);  //palabras no incluidas por ser muy grandes
+                }
+            }
+
+            // Close all the FileWriters
+            for (FileWriter writer : writers.values()) {
+                writer.close();
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(DividirDiccionario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void escribir(String linea) {
+        String letraInicial = linea.charAt(0) + "";
+        letraInicial = letraInicial.toLowerCase();
+
+        FileWriter writer = writers.get(letraInicial);
+        if (writer == null) {
+            try {
+                writer = new FileWriter(letraInicial);
+                writers.put(letraInicial, writer);
+            } catch (IOException ex) {
+                Logger.getLogger(DividirDiccionario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        try {
+            writer.write(linea);
+        } catch (IOException ex) {
+            Logger.getLogger(DividirDiccionario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
